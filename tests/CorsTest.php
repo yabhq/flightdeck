fffcf9910bccf75ee1b67cfb6dadea291fd27b8a
Jim <jimhlad@gmail.com>
2019-04-09 10:14:28 -0400

    Fix CORs test

--- tests/CorsTest.php
+++ tests/CorsTest.php
@@ -2,30 +2,20 @@
 
 namespace Yab\FlightDeck\Tests;
 
-use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Yab\FlightDeck\Models\User;
 use Yab\FlightDeck\Tests\TestCase;
-use Yab\FlightDeck\Http\Middleware\Cors;
 
 class CorsTest extends TestCase
 {
     /** @test */
-    public function cors_headers_are_set()
+    public function cors_headers_are_set_for_login_route()
     {
-        $response = \Mockery::Mock(Response::class)
-            ->shouldReceive('header')
-            ->with('Access-Control-Allow-Origin', '*')
-            ->shouldReceive('header')
-            ->with('Access-Control-Allow-Methods', 'HEAD, GET, PUT, PATCH, POST')
-            ->getMock();
-
-        $request = Request::create(route('login'), 'POST');
-
-        $middleware = new Cors;
-
-        $middleware->handle($request, function () use ($response) {
-            return $response;
-        });
+        $this
+            ->post(route('login'))
+            ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertHeader('Access-Control-Allow-Origin', '*')
+            ->assertHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+            ->assertHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization, Cache-Control, X-Requested-With');
     }
 }
